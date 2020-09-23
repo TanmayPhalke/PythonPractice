8a367ca71ed1d6bf3fbf045d5edde30208dd71f3
TanmayPhalke <31072498+TanmayPhalke@users.noreply.github.com>
2020-09-23 07:53:15 +0530

    Create leapyear.py

---
+++ leapyear.py
@@ -0,0 +1,14 @@
+year = int(input("Enter the year you want to check: "))
+if len(str(year)) == 4:
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                print("{0} is a leap year".format(year))
+            else:
+                print("{0} is not a leap year".format(year))
+        else:
+            print("{0} is a leap year".format(year))
+    else:
+        print("{0} is not a leap year".format(year))
+else:
+    print("Enter the year in correct format :')")
